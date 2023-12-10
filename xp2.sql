06cfb75bf9585159b2b1870e60a8d740f7a6ec71
anaishrb <152068628+anaishrb@users.noreply.github.com>
2023-12-10 19:43:43 +0200

    Add files via upload

--- xp2.sql
+++ xp2.sql
@@ -5,15 +5,26 @@
 --    birth_date date
 --);
 
-INSERT INTO students (id, first_name, last_name, birth_date) VALUES
-    (1, 'Marc', 'Benichou', TO_DATE('02/11/1998', 'DD/MM/YYYY')),
-    (2, 'Yoan', 'Cohen', TO_DATE('03/12/2010', 'DD/MM/YYYY')),
-    (3, 'Lea', 'Benichou', TO_DATE('27/07/1987', 'DD/MM/YYYY')),
-    (4, 'Amelia', 'Dux', TO_DATE('07/04/1996', 'DD/MM/YYYY')),
-    (5, 'David', 'Grez', TO_DATE('14/06/2003', 'DD/MM/YYYY')),
-    (6, 'Omer', 'Simpson', TO_DATE('03/10/1980', 'DD/MM/YYYY')),
+--INSERT INTO students (id, first_name, last_name, birth_date) VALUES
+--    (1, 'Marc', 'Benichou', TO_DATE('02/11/1998', 'DD/MM/YYYY')),
+--    (2, 'Yoan', 'Cohen', TO_DATE('03/12/2010', 'DD/MM/YYYY')),
+--    (3, 'Lea', 'Benichou', TO_DATE('27/07/1987', 'DD/MM/YYYY')),
+--    (4, 'Amelia', 'Dux', TO_DATE('07/04/1996', 'DD/MM/YYYY')),
+--    (5, 'David', 'Grez', TO_DATE('14/06/2003', 'DD/MM/YYYY')),
+--    (6, 'Omer', 'Simpson', TO_DATE('03/10/1980', 'DD/MM/YYYY')),
 	
 SELECT first_name, last_name FROM students;
+SELECT first_name, last_name FROM students WHERE id = 2;
+SELECT first_name, last_name FROM students WHERE last_name = 'Benichou' AND first_name = 'Marc';
+SELECT first_name, last_name FROM students WHERE last_name = 'Benichou' OR first_name = 'Marc';
+SELECT first_name, last_name FROM students WHERE first_name LIKE '%a%';
+SELECT first_name, last_name FROM students WHERE first_name LIKE 'a%';
+SELECT first_name, last_name FROM students WHERE first_name LIKE '%a';
+SELECT first_name, last_name FROM students WHERE first_name LIKE '%a%';
+SELECT first_name, last_name FROM students WHERE first_name LIKE 'a%';
+SELECT first_name, last_name FROM students WHERE first_name LIKE '%a';
+SELECT first_name, last_name FROM students WHERE SUBSTRING(first_name, LENGTH(first_name) - 1, 1) = 'a';
+SELECT first_name, last_name FROM students WHERE id IN (1, 3);
 
 
 
